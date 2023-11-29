5fbbba81494fc478d45848ebd8f79bfd2ffefba9
Ani Obinna Benzeal <anibenzealobinna@gmail.com>
2023-11-29 09:27:06 +0100

    "Inventing is a combination of brains and materials. The more brains you use, the less material you need"

--- 0x01-python-if_else_loops_functions /6-print_comb3.py
+++ 0x01-python-if_else_loops_functions /6-print_comb3.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-# Author - Tolulope Fakunle
-
+# Author - Ani Obinna Benzeal
 for digit1 in range(0, 10):
     for digit2 in range(digit1 + 1, 10):
         if digit1 == 8 and digit2 == 9:
