7cca2a5a80c9a869d5d7c95e8f924be82d680101
Ani Obinna Benzeal <anibenzealobinna@gmail.com>
2023-11-29 09:23:35 +0100

    "Hexadecimal printing"

--- 0x01-python-if_else_loops_functions /4-print_hexa.py
+++ 0x01-python-if_else_loops_functions /4-print_hexa.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 # Author - Ani Obinna Benzeal
-
 """Print numbers 0 to 98 in decimal and hexadecimal."""
 for number in range(0, 99):
     print("{} = {}".format(number, hex(number)))
