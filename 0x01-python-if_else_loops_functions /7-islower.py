022f090c05bd27c12e0f4f04c53a0e0e38c6e90c
Ani Obinna Benzeal <anibenzealobinna@gmail.com>
2023-11-29 09:17:30 +0100

    "islower"

---
+++ 0x01-python-if_else_loops_functions /7-islower.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# Author - Ani Obinna Benzeal
+
+def islower(c):
+    """Function checks for lowercase characters."""
+    if ord(c) >= 97 and ord(c) <= 122:
+        return True
+    else:
+        return False
